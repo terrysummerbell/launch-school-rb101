55f6fe3d3a29a8ee4344c9a6746b65bcce121e0a
Terry Summerbell <terrysummerbell@terrys-mbp.home>
2020-06-02 11:29:09 +0100

    calculator.rb update

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -4,27 +4,27 @@
 # output the result
 
 # answer = Kernel.gets()
-# Kernel.puts(answer) 
+# Kernel.puts(answer)
 
-Kernel.puts("Welcome to calculator!")
+Kernel.puts('Welcome to calculator!')
 
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp()
+Kernel.puts('What\'s the first number?')
+number1 = Kernel.gets.chomp
 
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp()
+Kernel.puts('What\'s the second number?')
+number2 = Kernel.gets.chomp
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) division")
-operator = Kernel.gets().chomp()
+Kernel.puts('What operation would you like to perform? 1) add 2) subtract 3) multiply 4) division')
+operator = Kernel.gets.chomp
 
 if operator == '1'
-  result = number1.to_i() + number2.to_i()
+  result = number1.to_i + number2.to_i
 elsif operator == '2'
-  result = number1.to_i() - number2.to_i() 
+  result = number1.to_i - number2.to_i
   elsif operator == '3'
-    result = number1.to_i() * number2.to_i()  
+    result = number1.to_i * number2.to_i
   else
-    result = number1.to_f() / number2.to_f()     
+    result = number1.to_f / number2.to_f
 end
 
-Kernel.puts("The result is #{result}")
+Kernel.puts("The result is #{result}") dfs
